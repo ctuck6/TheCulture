f3ac8a450eb25efeb16e49d5f3c41a81220af835
Cameron Tucker <cameronrtucker21@gmail.com>
2018-11-10 17:19:47 -0500

    change os.environ[] to .get()

--- app/config.py
+++ app/config.py
@@ -2,16 +2,16 @@ import os
 
 class Config():
 	DEBUG = True # should be false
-	SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI_LOCAL"]
+	SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI_LOCAL")
 	WHOOSH_BASE = "whoose"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SECRET_KEY = os.environ["SECRET_KEY"] or "you-will-never-guess"
-	WTF_CSRF_SECRET_KEY = os.environ["WTF_CSRF_SECRET_KEY"] or "you-will-never-guess"
-	MAIL_SERVER = os.environ["MAIL_SERVER"]
+	SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+	WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_SECRET_KEY") or "you-will-never-guess"
+	MAIL_SERVER = os.environ.get("MAIL_SERVER")
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ["MAIL_USERNAME"]
-	MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
+	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 	@staticmethod
@@ -20,12 +20,12 @@ class Config():
 
 class TestConfig(Config):
 	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI_TEST"]
+	SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI_TEST")
 	PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 class ProductionConfig(Config):
 	DEBUG = False
-	SQLALCHEMY_DATABASE_URI = os.environ["HEROKU_POSTGRESQL_COBALT_URL"]
+	SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_COBALT_URL")
 
 config = {
     "testing": TestConfig,
