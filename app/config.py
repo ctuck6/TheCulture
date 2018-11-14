67625cea0e2e90cea7c43d2586ac944b2d7e1286
Cameron Tucker <cameronrtucker21@gmail.com>
2018-11-14 17:56:45 -0500

    added mobile overlay navigation view

--- app/config.py
+++ app/config.py
@@ -12,6 +12,7 @@ class Config():
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+	SESSION_COOKIE_SECURE = False
 
 
 	@staticmethod
